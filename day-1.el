c4f8e412938c52fe794a4691695d80ba7a9c4d02
Adam Hall <adamhalldesigns@gmail.com>
2020-12-01 11:19:57 +0000

    setup passing test structure

---
+++ day-1.el
@@ -0,0 +1,12 @@
+;;; day-1.el --- Day 1 (advent of code 2020)
+
+;;; Commentary:
+
+;;; Code:
+(defun day-1-square (num)
+  "Square provided NUM."
+  (* num num))
+
+(provide 'day-1)
+
+;;; day-1.el ends here
