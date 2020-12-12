f7a4c9c71e154c9716ce94ca55eb0f498ae40907
Adam Hall <adamhalldesigns@gmail.com>
2020-12-12 11:28:56 +0000

    optimise solution and handle unhappy paths

--- day-1.el
+++ day-1.el
@@ -1,4 +1,4 @@
-;;; day-1.el --- Day 1 (advent of code 2020)
+;;; day-1.el --- Day 1 (advent of code 2020) -*- lexical-binding: t -*-
 
 ;;; Commentary:
 
@@ -10,24 +10,26 @@
 (defun day-1 (list)
   (-> list
       (my-string-to-list)
-      (get-sum-2020-from-list)
+      (get-tuple-that-sums-to-2020)
       (get-product)))
 
-(defun get-sum-2020-from-list (list)
-  "finds the two numbers in a list that sum to 2020"
-  (setq match nil)
-  (dolist (elm list match)
-    (dolist (inner-elm list)
-      (unless (= elm inner-elm)
-	(if (equal (+ elm inner-elm) 2020)
-	    (setq match (list elm inner-elm)))))))
+(defun get-tuple-that-sums-to-2020 (list)
+  "find first two values in LIST that sum to 2020 or return nil"
+  (pcase list
+    (`() ())
+    (`(,head) ())
+    (`(,head . ,tail)
+     (-if-let (match-number (-find (-partial 'sum-2020? head) tail))
+       (list head match-number)
+       (get-tuple-that-sums-to-2020 tail)))))
 
-(defun sum-2020 (n1 n2)
+(defun sum-2020? (n1 n2)
   "Do N1 and N2 sum to 2020."
-  (= 2020 (+ n1 n2)))
+  (message "testing %d %d" n1 n2) (= 2020 (+ n1 n2)))
 
 (defun get-product (list)
-  (-reduce '* list))
+  (when list 
+    (-reduce '* list)))
 
 (defun my-string-to-list (list)
   "Convert LIST of newline seperated numebrs into list."
@@ -48,6 +50,15 @@
 299
 675
 1456"))))
+
+(ert-deftest day-1-test--unhappy-paths ()
+  (should (eql '() (day-1 "1 2 3")))
+  (should (eql '() (day-1 "")))
+  (should (eql '() (day-1 "not a number")))
+  (should (eql '() (day-1 "1
+2
+3
+4"))))
 
 (day-1 (read-file "./inputs/day-1.txt"))
 ;; 1005459
