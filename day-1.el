2e63177d9c7fcea1e07be56c7159e274b215759f
Adam Hall <adamhalldesigns@gmail.com>
2020-12-12 15:39:01 +0000

    add part2

--- day-1.el
+++ day-1.el
@@ -12,16 +12,35 @@
       (get-tuple-that-sums-to 2020 it)
       (-reduce '* it)))
 
-(defun get-tuple-that-sums-to (target list)
+(defun day-1-part2 (list)
+  (-some--> list
+    (my-string-to-list it)
+    (get-three-tuple-that-sums-to 2020 it)
+
+    (-reduce '* it)))
+
+;;;; helpers
+
+(defun get-three-tuple-that-sums-to (target list)
   "find first two values in LIST that sum to 2020 or return nil"
-  (let ((sums-to-2020? (-partial 'sum-to target)))
-    (pcase list
-      (`() ())
-      (`(,head) ())
-      (`(,head . ,tail)
-       (-if-let (match (--find (funcall sums-to-2020? head it) tail))
-	   (list head match)
-	 (get-tuple-that-sums-to target tail))))))
+  (pcase list
+    (`() ())
+    (`(,h1) ())
+    (`(,h1 . (,h2 . ,tail)) (-if-let (match (get-tuple-that-sums-to target (cons h2 tail) h1))
+				(cons h1 match)
+			      (get-three-tuple-that-sums-to target (cons h2 tail))))
+    (`(,head . ,tail) ())))
+
+(defun get-tuple-that-sums-to (target list &optional seed)
+  "find first two values in LIST that sum to 2020 or return nil"
+  (unless seed (setq seed 0))
+  (pcase list
+    (`() ())
+    (`(,head) ())
+    (`(,head . ,tail)
+     (-if-let (match (--find (equal target (+ seed head it)) tail))
+	 (list head match)
+       (get-tuple-that-sums-to target tail seed)))))
 
 (defun sum-to (target n1 n2)
   "Do N1 and N2 sum to 2020."
@@ -37,7 +56,7 @@
     (insert-file-contents file)
     (buffer-string)))
 
-;; Tests;
+;;;; Tests;
 
 (ert-deftest day-1-test--given-example ()
   (should (= 514579 (day-1 "1721
@@ -54,9 +73,35 @@
   (should (eql '() (day-1 "1
 2
 3
-4"))))
+4
+2020"))))
+
+(ert-deftest day-1-part2-test--given-example ()
+  (should (= 241861950 (day-1-part2 "1721
+979
+366
+299
+675
+1456"))))
+
+(ert-deftest day-1-part-2-test--unhappy-paths ()
+  (should (eql '() (day-1-part2 "1 2 3")))
+  (should (eql '() (day-1-part2 "")))
+  (should (eql '() (day-1-part2 "not a number")))
+  (should (eql '() (day-1-part2 "1
+2
+3
+4
+2020
+2019"))))
 
 (message "%s" (day-1 (read-file "./inputs/day-1.txt")))
 ;; 1005459
+
+;; obviously need to work on why I need this :P
+(setq max-lisp-eval-depth 10000)
+(setq max-specpdl-size 10000)
+(message "%s" (day-1-part2 (read-file "./inputs/day-1.txt")))
+;; 92643264
 
 ;;; day-1.el ends here
